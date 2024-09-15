1ca02a32d52b995c49e0eed78b8cda8a70791ba0
Garrison Wright <177910142+7-garrison-7@users.noreply.github.com>
2024-09-14 22:26:04 -0400

    Simplify code

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,15 +1,10 @@
 const repeatString = function(string, num) {
-    
-    if (num >= 0) {
-        const repeatedString = new Array(num); 
-        for (let i = 0; i < num; i++) {
-            repeatedString[i] = string;
-        } 
-        return repeatedString.join(""); 
-    } else {
-        return "ERROR";
+    if (num < 0) return "ERROR";
+    let repeatedString = ''; 
+    for (let i = 0; i < num; i++) {
+        repeatedString += string;
     }
-
+    return repeatedString;
 };
 
 // Do not edit below this line
