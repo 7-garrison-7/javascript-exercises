8ead5fde25c1b7c2170725e44a3ba7e9bbbbc6a1
Garrison Wright <177910142+7-garrison-7@users.noreply.github.com>
2024-09-14 22:22:30 -0400

    Arrive at first draft that passes all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    
+    if (num >= 0) {
+        const repeatedString = new Array(num); 
+        for (let i = 0; i < num; i++) {
+            repeatedString[i] = string;
+        } 
+        return repeatedString.join(""); 
+    } else {
+        return "ERROR";
+    }
 
 };
 
