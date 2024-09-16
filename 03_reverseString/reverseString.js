0d9fd9ee835bea6c404b6a8a88e5445df4282034
Garrison Wright <177910142+7-garrison-7@users.noreply.github.com>
2024-09-15 21:51:28 -0400

    Create function. Satisfy test 1

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(originalString) {
+    let reversedString = '';
 
+    for (let i = 0; i < originalString.length; i++) {
+        reversedString += originalString.substring((originalString.length-1)-i, (originalString.length)-i);
+    }
+
+    return reversedString;
 };
 
 // Do not edit below this line
