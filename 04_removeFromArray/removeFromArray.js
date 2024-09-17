73a8506818dbf6982ec6856dcaa4225688f4ba3d
Garrison Wright <177910142+7-garrison-7@users.noreply.github.com>
2024-09-16 23:20:00 -0400

    Complete first draft for test 1

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrayInput, elementTarget) {
+    for (element in arrayInput) {
+        if (element == elementTarget) {
+            arrayInput.slice(arrayInput.indexOf(element), 1);
+        }
+    }
+    return arrayInput;
 };
 
 // Do not edit below this line
