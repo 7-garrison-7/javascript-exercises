7415db543e581ce15ffcbe268660591c28ac4794
Garrison Wright <177910142+7-garrison-7@users.noreply.github.com>
2024-09-18 21:22:16 -0400

    Add test to terminate if given non-positive integers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,9 @@
 const sumAll = function(first, last) {
+    for (arg of arguments) {
+        if (!Number.isInteger(arg) || Math.sign(arg) == -1) {
+            return("ERROR");
+        }
+    }
     let sum = 0;
     let numArray = [];
     const small = first < last ? first : last;
