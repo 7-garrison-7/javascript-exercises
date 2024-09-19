f1a98cabbfe59dc2ab4165c3a1612c9b7da490b5
Garrison Wright <177910142+7-garrison-7@users.noreply.github.com>
2024-09-18 20:57:43 -0400

    First draft of function that passes first two tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(first, last) {
+    let sum = 0;
+    let numArray = [];
+    const small = first < last ? first : last;
+    const big = first < last ? last : first;
+    for (let i = 0; small+i <= big; i++) {
+        numArray.push(small+i);
+    }
+    numArray.forEach((num) => {
+        sum += num;
+    })
+    return(sum);
 };
 
 // Do not edit below this line
