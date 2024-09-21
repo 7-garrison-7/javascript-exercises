a105fb1c836593e42af79cd6845aaa12f3ceacab
Garrison Wright <177910142+7-garrison-7@users.noreply.github.com>
2024-09-20 21:10:39 -0400

    First draft of functions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+
+  // Formula to Celsius is:
+  // (x - 32) * (5 / 9)
+  let result = ((temp - 32) * (5 / 9));
+
+  result = number.toFixed(1);
+  return parseInt(result);
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+
+  // Formula to Fahrenheit is:
+  // (x * (9/5) + 32)
+  let result = (temp * (9 / 5 + 32));
+
+  result = number.toFixed(1);
+  return parseInt(result);
+
 };
 
 // Do not edit below this line
