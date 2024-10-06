0ec8626e0ad77490630d42c76347c926ea8cf58d
Garrison Wright <177910142+7-garrison-7@users.noreply.github.com>
2024-10-05 21:25:24 -0400

    Code palindromes func

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const regEx = /\W/g;
+    const stringFormatted = string.toLowerCase().replaceAll(regEx, '');
+    const stringReversed = stringFormatted.split('').reverse().join('');
+    return stringFormatted == stringReversed;
 };
 
 // Do not edit below this line
