2dcf719d4f7ba25c109ff3d96acda477f610ba1b
Garrison Wright <177910142+7-garrison-7@users.noreply.github.com>
2024-10-06 20:32:01 -0400

    Code fibonacci fn

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(pos) {
+    if (pos < 0) {
+        return "OOPS";
+    }
+    console.log(`Position arg = ${pos}`);
+    let array = [0, 1, 1];
+    console.log(`Starting array = ${array}`);
+    for (let i = 0; i < pos; i++) {
+        console.log(`Array at -1 = ${array.at(-1)}`);
+        console.log(`Array at -2 = ${array.at(-2)}`);
+        console.log(`Sum = ${array.at(-1) + array.at(-2)}`)
+        console.log("Pushing to array");
+        array.push(array.at(-1) + array.at(-2));
+        console.log(array);
+    }
+    console.log(array[pos]);
+    return array[pos];
 };
 
 // Do not edit below this line
