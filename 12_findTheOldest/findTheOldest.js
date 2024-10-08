cff5616808346564c8b4079cd086a014fa1365af
Garrison Wright <177910142+7-garrison-7@users.noreply.github.com>
2024-10-08 14:59:41 -0400

    Code fn

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,7 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    return(array).sort((a, b) => (
+        (b.yearOfDeath == undefined ? new Date().getFullYear() : b.yearOfDeath) - b.yearOfBirth) - 
+        ((a.yearOfDeath == undefined ? new Date().getFullYear() : a.yearOfDeath) - a.yearOfBirth))[0];
 };
 
 // Do not edit below this line
